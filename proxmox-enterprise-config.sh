4b48f1192e41d5f7c4867b6c2f13a58776034790
weidmarkgit <97971892+weidmarkgit@users.noreply.github.com>
2022-02-15 14:01:55 -0500

    refactored and localized functions

--- proxmox-enterprise-config.sh
+++ proxmox-enterprise-config.sh
@@ -1,52 +1,81 @@
 #!/bin/bash
-
-SOURCES=/etc/apt/sources.list 
-PVE_ENTERPRISE=/etc/apt/sources.list.d/pve-enterprise.list
-PBS_ENTERPRISE=/etc/apt/sources.list.d/pbs-enterprise.list
-
 IS_PBS=false
 IS_PVE=false
 
+function detect-version() {
+    
+    # Checks for enterprise repository source list
+    # Deletes the enterprise repostiory source list
+    # Detects version based on repository source list generated
 
-#Check for APT sources file
-if test -f "$SOURCES"; then
-    rm -rfv $SOURCES
-else
-    echo "$SOURCES does not exist"
-fi
+    local -r  pve_enterprise=/etc/apt/sources.list.d/pve-enterprise.list
+    local -r  pbs_enterprise=/etc/apt/sources.list.d/pbs-enterprise.list
 
-#Check for Proxmox VE enterprise repository file
-if test -f "$PVE_ENTERPRISE"; then
-    echo "$PVE_ENTERPRISE exists and and will be removed"
-    rm -rfv $PVE_ENTERPRISE
-    IS_PVE=true
-else
-    echo "#PVE_ENTERPRISE does not exist"
-fi
+    #Check for Proxmox VE enterprise repository file
+    if test -f "${pve_enterprise}"; then
+        rm --recursive --force --verbose ${pve_enterprise}
+        IS_PVE=true
+    else
+        echo "${pve_enterprise} does not exist"
+    fi
 
-#Check for Proxmox Backup enterprise repository file
-if test -f "$PBS_ENTERPRISE"; then
-    echo "$PBS_ENTERPRISE exists and and will be removed"
-    rm -rfv $PBS_ENTERPRISE
-    IS_PBS=true
-else
-    echo "$PBS_ENTERPRISE does not exist"
-fi
+    #Check for Proxmox Backup enterprise repository file
+    if test -f "${pbs_enterprise}"; then
+        rm --recursive --force --verbose ${pbs_enterprise}
+        IS_PBS=true
+    else
+        echo "${pbs_enterprise} does not exist"
+    fi
 
-#Create new APT source list
-cp /root/pve-tools/sources.list /etc/apt/
+}
 
-#Adding fake enterprise repository
-wget https://github.com/Jamesits/pve-fake-subscription/releases/download/v0.0.7/pve-fake-subscription_0.0.7_all.deb
-dpkg -i pve-fake-subscription_0.0.7_all.deb
-echo "127.0.0.1 shop.maurer-it.com" | tee -a /etc/hosts
-rm pve-fake-subscription_0.0.7_all.deb
+function configure-sources() {
 
-#Final touches
+    # Removes default source list
+    # Replaces source list based on Proxmox installation
+    local -r  sources=/etc/apt/sources.list 
+    local -r  custom_pbs=/root/pve-tools/custom-sources/pbs-sources.list
+    local -r  custom_pve=/root/pve-tools/custom-sources/pve-sources.list
+
+    #Check for APT sources file
+    if test -f "${sources}"; then
+        rm --recursive --force --verbose ${sources}
+    else
+        echo "${sources} does not exist"
+    fi
+
+    if ${IS_PBS}; then
+        cp ${custom_pbs} /etc/apt/
+        mv /etc/apt/pbs-sources.list /etc/apt/sources.list 
+    elif ${IS_PVE}; then 
+        cp ${custom_pve} /etc/apt/
+        mv /etc/apt/pve-sources.list /etc/apt/sources.list
+    else 
+        printf "####\n## Unable to determine Proxmox installation type\n####\n" 
+    fi 
+
+}
+
+function configure-placeholder-subscription() {
+
+    local -r placeholder_subscription_url=https://github.com/Jamesits/pve-fake-subscription/releases/download/v0.0.7/pve-fake-subscription_0.0.7_all.deb
+    
+    wget ${placeholder_subscription_url}
+    dpkg -i pve-fake-subscription_0.0.7_all.deb
+    echo "127.0.0.1 shop.maurer-it.com" | tee -a /etc/hosts
+    rm pve-fake-subscription_0.0.7_all.deb
+
+}
+
+# Function calls
+detect-version
+configure-sources
+configure-placeholder_subscription_url
+
 apt update
 apt upgrade -y
 
-if $IS_PVE; then 
+if ${IS_PVE}; then 
     pveam update
 fi
 
