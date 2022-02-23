319e0304357b091a0aaa4f7471928da05e7d567f
weidmarkgit <97971892+weidmarkgit@users.noreply.github.com>
2022-02-23 12:21:44 -0500

    refactored for MIB

--- postfix-config.sh
+++ postfix-config.sh
@@ -1,31 +1,47 @@
 #!/bin/bash
+CLIENT_EMAIL=""
 
-#Install the authentication library
-apt install libsasl2-modules -y
+function initial-configuration(){
+  local -r client_password
+  read -r -p "Please enter client email: " CLIENT_EMAIL
+  read -r -s -p "Please enter client password: " client_password
 
-#Configure our API key for login
-echo "INSERT SENDGRID KEY HERE AND DO NOT PUSH IT TO ORIGIN . . . EVER" > /etc/postfix/sasl_passwd
+  #Install the authentication library
+  apt install libsasl2-modules bsd-mailx -y
 
-#Stored hashed API key into database
-postmap hash:/etc/postfix/sasl_passwd
-chmod 600 /etc/postfix/sasl_passwd
+  #Create password file
+  echo "[box.weidmark.support]:465 ${CLIENT_EMAIL}:${client_password}" > /etc/postfix/sasl_passwd
 
-#Backup and overwrite main.cf configuration file
-cp /etc/postfix/main.cf /etc/postfix/main.cf.backup
-cp --force /root/pve-tools/postfix-files/main.cf /etc/postfix/
+  #Stored hashed API key into database
+  postmap hash:/etc/postfix/sasl_passwd
+  chmod 600 /etc/postfix/sasl_passwd
 
-#Overwrite sender ID
-echo "/.+/    support@weidmarkit.com" > /etc/postfix/sender_canonical_maps
-#Overwrite header ID
-echo "/From:.*/ REPLACE From: support@weidmarkit.com" > /etc/postfix/header_check
-#Reload postfix
-postfix reload
+  #Backup and overwrite main.cf configuration file
+  cp /etc/postfix/main.cf /etc/postfix/main.cf.backup
+  cp --force /root/pve-tools/postfix-files/main.cf /etc/postfix/
+}
 
-#Install library if using proxmox backup server
-if [ -x "$(command -v proxmox-backup-client)" ] ; then
-    apt install bsd-mailx -y
-fi
+function id-configuration(){
+  #Overwrite sender ID
+  echo "/.+/    ${CLIENT_EMAIL}" > /etc/postfix/sender_canonical_maps
+  #Overwrite header ID
+  echo "/From:.*/ REPLACE From: ${CLIENT_EMAIL}" > /etc/postfix/header_check
+  #Reload postfix
+  postfix reload
+}
+function test-email(){
+  #You have mail
+  echo "test message" | mail -s "testing ${CLIENT_EMAIL}" support@weidmarkit.com
+  echo "Test message sent to support@weidmarkit.com"
+}
 
-#You have mail
-echo "test message" | mail -s "test subject" support@weidmarkit.com
-echo "Test message sent to support@weidmarkit.com"
+initial-configuration
+id-configuration
+test-email
+
+finish() {
+  result=$?
+    printf "postfixconfig.sh completed succesfully\n Please reboot your system to complete configuration\n"
+  exit ${result}
+}
+trap finish EXIT ERR
